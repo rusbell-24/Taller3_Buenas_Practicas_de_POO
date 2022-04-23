5b480bd57577d30837bc24c805ca8116ef63a330
Rusbell Ruiz Portocarrero <65029620+rusbell-24@users.noreply.github.com>
2022-04-23 18:30:51 -0500

    Actulización de la rama "branch_rusbell" con rama "main"
    Se actualizo la rama "branch_rusbell" con todo el contenido actualizado en la rama "main". Con la finalidad de hacer cambios experimentales al proyecto desde la rama "branch_rusbell".

---
+++ README.md
@@ -0,0 +1,66 @@
+# TALLER 3 - BUENAS PRACTICAS DE POO
+
+## INTRODUCCION
+
+Este proyecto tiene como finalidad cumplir con los requisitos mínimos exigidos para la entrega del tercer taller (Buenas prácticas de programación y manejo de excepciones). Para tal fin, se realiza un proceso de análisis de la información con el objetivo de extraer datos relevantes que permitan diseñar una aplicación que administre una lista de canciones y que adicionalmente cumpla con los siguientes requisitos:
+
+1. Tener una biblioteca principal con todas las canciones.
+
+2. Tener una funcionalidad que permita crear playlist con canciones determinadas.
+
+3. Cada canción debe tener el título, identificador, fecha, duración, genero, caratula y una descripción corta.
+
+4. Se deben crear comportamientos en donde se permita hacer filtros a la biblioteca principal, los filtros son:
+	- Filtrar canciones del mismo género.
+	- Filtrar canciones en el mismo año.
+5. Se debe crear comportamientos en donde se permita ordenar por duración y fecha.
+
+## DESCRIPCION
+
+Se llevó a cabo un proceso de abstracción por medio del cual se detectaron aspectos específicos que describen el comportamiento de un reproductor de música.
+
+Gracias al proceso de abstracción anteriormente mencionado, se logró que el reproductor de música desarrollado en el presente proyecto ofrezca las siguientes funcionalidades:
+
+### Funcionalidades Del Reproductor
+A continuación, se puntualizan cada una de las funciones que ofrece el reproductor:
+
+1. Listar la biblioteca general de canciones.
+2. Reproducir la biblioteca general de canciones.
+3. Crear playlist a partir de la biblioteca general de canciones.
+	- Ver playlist.
+	- Reproducir playlist.
+4. Ordenar la Biblioteca general de canciones:
+	- Ordenar por generos.
+	- Ordenar alfabeticamente por titulo de la canción.
+	- Ordenar por duración.
+	- Ordenar por año.
+5. Reproducir lista ordenada.
+6. Crear playlist a partir de la lista ordenada.
+	- Ver playlist.
+	- Reproducir playlist.
+6. Filtrar canciones por género.
+7. Reproducir genero filtrado.
+
+### Clases Implementadas
+- Main (Clase Main)
+- SongsLibrary (Clase Abstracta)
+- PlayList
+- SortBy
+
+## TEMAS
+
+En el presente proyecto se elaboraron 4 clases de la cuales 1 es abstracta. En estas se abordan algunos temas de la programación orientada a objetos, tales como:
+
+- Clases.
+- Objetos.
+- Atributos.
+- Métodos.
+- Modificadores de acceso.
+- Método constructor.
+- Abstracciones.
+- Polimorfismo.
+- Manejo de excepciones (try-catch).
+- Documentación.
+- Buenas prácticas de programación, etc..
+
+Finalmente, la totalidad de las clases se encuentran en el package ***edu.musicplayer*** .
--- src/edu/musicplayer/Main.java
+++ src/edu/musicplayer/Main.java
@@ -1,17 +1,18 @@
 package edu.musicplayer;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
-
     public static void main(String[] args){
 
         String firstLevelOptions;
         String secondLevelOptions;
         String thirdLevelOptions;
         boolean exit = false;
+        List<ArrayList<String>> dinamicArray = new ArrayList<>();
+
         Scanner userData = new Scanner(System.in);
 
         do{
@@ -27,11 +28,12 @@ public class Main {
             firstLevelOptions = userData.next();
 
             SortBy seeLibrary = new SortBy();
-
+            dinamicArray = seeLibrary.getSongList();
+            seeLibrary.setLibraryToShowToSongList();
             if (firstLevelOptions.equals("1")) {
                 seeLibrary.setLibraryToShowToSongList();
-                for (ArrayList cancion : seeLibrary.getSongList()) {
-                    System.out.println(cancion);
+                for (ArrayList song : seeLibrary.getSongList()) {
+                    System.out.println(song);
                 }
             }else if (firstLevelOptions.equals("2")) {
                 seeLibrary.setLibraryToShowToSongList();
@@ -51,18 +53,22 @@ public class Main {
                         seeLibrary.setToAlphabeticGenders();
                         seeLibrary.sortByGender();
                         seeLibrary.ascendentSort();
+                        dinamicArray = seeLibrary.getLibraryToShow();
                         break;
                     case "2":
                         seeLibrary.setToAlphabeticNames();
                         seeLibrary.ascendentSort();
+                        dinamicArray = seeLibrary.getLibraryToShow();
                         break;
                     case "3":
                         seeLibrary.setToSongDuration();
                         seeLibrary.ascendentSort();
+                        dinamicArray = seeLibrary.getLibraryToShow();
                         break;
                     case "4":
                         seeLibrary.setToYear();
                         seeLibrary.ascendentSort();
+                        dinamicArray = seeLibrary.getLibraryToShow();
                         break;
                 }
                 System.out.println("----------Opciones---------");
@@ -101,7 +107,7 @@ public class Main {
             System.out.println(firstLevelOptions);
             }if (firstLevelOptions.equals("4")) {
                 String namePlayList;
-                String song;
+                String idSong;
                 boolean createPlayList = false;
                 boolean finish = false;
 
@@ -110,19 +116,19 @@ public class Main {
                 PlayList newPlayList = new PlayList();
 
                 System.out.println("----Biblioteca General De Canciones-----");
-                for (ArrayList songElement : newPlayList.getSongList()) {
+                for (ArrayList<String> songElement : dinamicArray) {
                     System.out.println("=> Id: " + songElement.get(0) + " Titulo: " + songElement.get(1));
                 }
 
-                System.out.println("Nombre de la nueva playlis: ");
+                System.out.println("Nombre de la nueva playlist: ");
                 namePlayList = userData.next();
 
                 do {
                     System.out.println("Ingrese el id de la cancion a añadir o una letra para terminar: ");
-                    song = userData.next();
+                    idSong = userData.next();
 
-                    if (song.matches("[+-]?\\d*(\\.\\d+)?")) {
-                        idSongs.add(song);
+                    if (idSong.matches("[+-]?\\d*(\\.\\d+)?")) {
+                        idSongs.add(idSong);
                         createPlayList = true;
                     } else {
                         finish = true;
@@ -131,36 +137,41 @@ public class Main {
                 } while (!finish);
 
                 if (createPlayList) {
+                    try {
+                        newPlayList.addSongs(namePlayList, idSongs, dinamicArray);
+                        System.out.println("Has creado una playlist nueva...............");
 
-                    newPlayList.addSongs(namePlayList, idSongs);
-                    System.out.println("Has creado una playlist nueva...............");
+                        do {
+                            System.out.println("_____Opciones de PlayList_____");
+                            System.out.println("1. Ver");
+                            System.out.println("2. Reproducir ");
+                            System.out.println("3. Salir\n");
+                            System.out.println("Que deseas hacer: ");
 
-                    do {
+                            thirdLevelOptions = userData.next();
 
-                        System.out.println("_____Opciones de PlayList_____");
-                        System.out.println("1. Ver");
-                        System.out.println("2. Reproducir ");
-                        System.out.println("3. Salir\n");
+                            switch (thirdLevelOptions) {
+                                case "1":
+                                    ArrayList<ArrayList<String>> seeSongs = newPlayList.seePlayList();
+                                    System.out.println("_______Lista de reproduccion *** " + seeSongs.get(0) + "***_______");
+                                    for (int i = 1; i < seeSongs.size(); i++) {
+                                        System.out.println("id: " + seeSongs.get(i).get(0) + " titulo: " + seeSongs.get(i).get(1));
+                                    }
+                                    System.out.println("Fin lista..........");
+                                    break;
+                                case "2":
+                                    newPlayList.playSongList();
+                                    break;
 
-                        System.out.println("Que deseas hacer: ");
-                        thirdLevelOptions = userData.next();
-                        switch (thirdLevelOptions) {
-                            case "1":
-                                ArrayList<ArrayList<String>> seeSongs = newPlayList.seePlayList();
-                                System.out.println("_______Lista de reproduccion *** " + seeSongs.get(0) + "***_______");
-                                for (int i = 1; i < seeSongs.size(); i++) {
-                                    System.out.println("id: " + seeSongs.get(i).get(0) + " titulo: " + seeSongs.get(i).get(1));
-                                }
+                            }
+                        } while (!(thirdLevelOptions.equalsIgnoreCase("3")));
 
-                                System.out.println("Fin lista..........");
-                                break;
-                            case "2":
-                                newPlayList.playSongList();
-                                break;
-
-                        }
-                    } while (!(thirdLevelOptions.equalsIgnoreCase("3")));
-
+                    } catch (NullPointerException e) {
+                        System.out.println("\nEstamos presentando inconvenientes con la lista de Canciones:::::::::::::\n");
+                        System.out.println("INTENTE EN OTRO MOMENTO:::::::::::::\n");
+                    }
+                }else{
+                    System.out.println("\nNo seleccionaste ninguna canción......\n\n");
                 }
             }else if (firstLevelOptions.equals("5")) {
                 System.out.println("Apagando reproductor de musica......");
@@ -169,151 +180,6 @@ public class Main {
 
         }while (!exit);
 
-            /*
-            switch (option){
-                case "1" :
-                    seeLibrary.setLibraryToShowToSongList();
-                    for (ArrayList cancion: seeLibrary.getSongList()) {
-                        System.out.println(cancion);
-                    }
-                    break;
-                case "2" :
-                    seeLibrary.setLibraryToShowToSongList();
-                    seeLibrary.playSongList();
-                    break;
-                case "3" :
-                    seeLibrary.setLibraryToShowToSongList();
-                    String op1;
-                    System.out.println("_______Opciones______");
-                    System.out.println("1. Ordenar por generos");
-                    System.out.println("2. Ordenar alfabeticamente por nombre de cancion");
-                    System.out.println("3. Ordenar por duracion de cancion");
-                    System.out.println("4. Ordenar por año\n");
-                    System.out.println("Ingrese su opcion: ");
-                    op1 = userData.next();
-                    switch (op1){
-                        case "1":
-                            seeLibrary.setToAlphabeticGenders();
-                            seeLibrary.sortByGender();
-                            seeLibrary.ascendentSort();
-                            break;
-                        case "2":
-                            seeLibrary.setToAlphabeticNames();
-                            seeLibrary.ascendentSort();
-                            break;
-                        case "3":
-                            seeLibrary.setToSongDuration();
-                            seeLibrary.ascendentSort();
-                            break;
-                        case "4":
-                            seeLibrary.setToYear();
-                            seeLibrary.ascendentSort();
-                            break;
-                    }
-                    System.out.println("----------Opciones---------");
-                    System.out.println("1. Reproducir Lista ordenada");
-                    System.out.println("2.Reproducir solo salsa");
-                    System.out.println("3.Reproducir solo Rock en español");
-                    System.out.println("4.Reproducir solo Urbano latino");
-                    System.out.println("5. Crear playlist");
-                    System.out.println("6. salir\n");
 
-                    System.out.println("Ingrese su opcion: ");
-                    op1 = userData.next();
-                    switch (op1){
-                        case "1":
-                            seeLibrary.playSongList();
-                            break;
-                        case "2":
-                            seeLibrary.sortByGender();
-                            seeLibrary.setLibraryToShowToSalsa();
-                            seeLibrary.playSongList();
-                            break;
-                        case "3":
-                            seeLibrary.sortByGender();
-                            seeLibrary.setLibraryToShowToRock();
-                            seeLibrary.playSongList();
-                            break;
-                        case "4":
-                            seeLibrary.sortByGender();
-                            seeLibrary.setLibraryToShowToUrbano();
-                            seeLibrary.playSongList();
-                            break;
-                        case "5":
-                            option = "4";
-                            break;
-                    }
-
-                    break;
-                case "4" :
-                    String namePlayList;
-                    String song;
-                    boolean createPlayList = false;
-                    boolean finish = false;
-
-                    ArrayList<String> idSongs = new ArrayList<>();
-
-                    PlayList newPlayList = new PlayList();
-
-                    System.out.println("----Biblioteca General De Canciones-----");
-                    for (ArrayList songElement: newPlayList.getSongList()) {
-                        System.out.println("=> Id: " + songElement.get(0) + " Titulo: " + songElement.get(1));
-                    }
-
-                    System.out.println("Nombre de la nueva playlis: ");
-                    namePlayList = userData.next();
-
-                    do {
-                        System.out.println("Ingrese el id de la cancion a añadir o una letra para terminar: ");
-                        song = userData.next();
-
-                        if (song.matches("[+-]?\\d*(\\.\\d+)?")){
-                            idSongs.add(song);
-                            createPlayList = true;
-                        }else{
-                            finish = true;
-                        }
-
-                    }while (!finish);
-
-                    if (createPlayList){
-                        String op;
-                        newPlayList.addSongs(namePlayList, idSongs);
-                        System.out.println("Has creado una playlist nueva...............");
-
-                        do{
-
-                            System.out.println("_____Opciones de PlayList_____");
-                            System.out.println("1. Ver");
-                            System.out.println("2. Reproducir ");
-                            System.out.println("3. Salir\n");
-
-                            System.out.println("Que deseas hacer: ");
-                            op = userData.next();
-                            switch (op) {
-                                case "1":
-                                    ArrayList<ArrayList<String>> seeSongs = newPlayList.seePlayList();
-                                    System.out.println("_______Lista de reproduccion *** " + seeSongs.get(0) + "***_______");
-                                    for (int i = 1; i < seeSongs.size(); i++) {
-                                        System.out.println("id: " + seeSongs.get(i).get(0) + " titulo: " + seeSongs.get(i).get(1));
-                                    }
-
-                                    System.out.println("Fin lista..........");
-                                    break;
-                                case "2":
-                                    newPlayList.playSongList();
-                                    break;
-
-                            }
-                        }while(!(op.equalsIgnoreCase("3")));
-
-                    }
-                    break;
-                case "5":
-                    System.out.println("Apagando reproductor de musica......");
-                    exit = true;
-            }
-
-        }while (!exit);*/
     }
 }
