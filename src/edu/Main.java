c13fafaeffa9b0b3179d4775036c4601f640fc93
justinok <38015681+justinok@users.noreply.github.com>
2022-04-24 23:59:53 -0500

    Cambios en Main para documentacion

--- src/edu/Main.java
+++ src/edu/Main.java
@@ -5,17 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 
 /**
- * ------------------------------------IMPORTANTEEE------------------------------------
+ * ------------------------------------IMPORTANTEEE-----------------------------
  * Cambiar datos de version y de since, en toda la clase y los metodos
  */
 
 /**
- * En la clase Main se contruye toda la interfaz con la que el usuario va a interactuar
- * con la biblioteca y el reproductor de musica, tambien se contruye aqui la parte trasera
- * del sistema donde se indica que accion se tomara cuando el usuario solicite una determinada
- * funcion.
+ * En la clase Main se contruye toda la interfaz con la que el usuario va a
+ * interactuar con la biblioteca y el reproductor de musica, tambien se contruye
+ * aqui la parte trasera del sistema donde se indica que accion se tomara cuando
+ * el usuario solicite una determinada funcion.
  *
- * ex:  Interfaz de consola que permite al Usuario seleccionar X,Y y Z funciones:
+ * ex: Interfaz de consola que permite al Usuario seleccionar X,Y y Z funciones:
  *          Si Usuario selecciona funcion X {
  *                      Por favor sistema invoca la clase W y su metodo X }
  *          Si Usuario selecciona funcion Y {
@@ -35,6 +35,40 @@ public class Main {
     }
 
     public static void menu(){
+        /**
+         * @param firstLevelOptions,secondLevelOptions,thirdLevelOptions Strings
+         * Se crean variables que se usaran en el mecanismo de menus que se le
+         * mostrara al Usuario.
+         *
+         * Tenemos dos submenus por lo que se creo una variable para cada nivel
+         * de profundidad de menu
+         *
+         * En cada nivel se utiliza su determinada variable para realizar una
+         * determinada accion ya sea con un if else o con un switch case
+         *
+         * ex:  Menu principal:
+         *          if firstLevelOptions = 1 {
+         *                  Muestre Segundo Menu:
+         *                          if secondLevelOptions = 1 {
+         *                                  Muestre Tercer Menu:
+         *                                      - opcion 1
+         *                                      - opcion 2
+         *                                      - etc..
+         *                           }
+         *          }
+         *          if firstLevelOptions = 2 {
+         *                  boolean exit = True;
+         *                  Salga del sistema;
+         *          }
+         *
+         * @param exit boolean
+         * Se utilizo una lista de ArrayList dinamica, dinamicArray, para poder
+         * lograr versatilidad entre las funciones del menu y los metodos de
+         * diferentes clases
+         *
+         * @param userData
+         * Variable para guardar la respuesta del Usuario a un dado menu
+         */
         String firstLevelOptions;
         String secondLevelOptions;
         String thirdLevelOptions;
@@ -46,22 +80,25 @@ public class Main {
         Scanner userData = new Scanner(System.in);
 
         /**
-         * Se utiliza un Do para crear el ciclo de interfaz grafica para el Usuario e ingresar al
-         * primer menu o menu principal
+         * Se utiliza un Do para crear el ciclo de interfaz grafica para el
+         * Usuario e ingresar al primer menu o menu principal
          */
         do{
             showMenu();
             /**
              * Se registra la respuesta del Usuario al menu principal
-             * Se crea un objeto seeLibrary de la clase sortBy que nos ayudara a organizar las canciones
-             * Se utiliza un if para evaluar cual fue la repsuesta del usuario e iniciar una determinada
-             * cadena de acciones.
+             * Se crea un objeto seeLibrary de la clase sortBy que nos ayudara a
+             * organizar las canciones
+             * Se utiliza un if para evaluar cual fue la repsuesta del usuario e
+             * iniciar una determinada cadena de acciones.
              */
             firstLevelOptions = userData.next();
             SortBy seeLibrary = new SortBy();
 
             dinamicArray = seeLibrary.getSongList();
-
+            /**
+             * Usuario escribe 1 -> Ver biblioteca general
+             */
             if (firstLevelOptions.equals("1")) {
                 System.out.println(
                         "________Biblioteca General De Canciones______\n"
@@ -71,10 +108,16 @@ public class Main {
                     System.out.println(song);
                 }
             }
+            /**
+             * Usuario escribe 2 -> Reproducir Biblioteca
+             */
             else if (firstLevelOptions.equals("2")) {
                 seeLibrary.setLibraryToShowToSongList();
                 seeLibrary.playSongList();
             }
+            /**
+             * Usuario escribe 3 -> Ordenar canciones e ingresar al segundo menu
+             */
             else if (firstLevelOptions.equals("3")) {
 
                 seeLibrary.setLibraryToShowToSongList();
@@ -84,10 +127,12 @@ public class Main {
                 secondLevelOptions = userData.next();
 
                 /**
-                 * Se utiliza un switch case para evaluar que acciones solicito el Usuario y que
-                 * acciones debe tomar el Usuario en este segundo nivel
-                 * Las acciones solicitadas aqui solo organizaran los datos, luego el usuario debe
-                 * definir que accion se tomara con esas canciones organizadas.
+                 * Se utiliza un switch case para evaluar que acciones solicito
+                 * el Usuario y que acciones debe tomar el Usuario en este
+                 * segundo nivel
+                 * Las acciones solicitadas aqui solo organizaran los datos
+                 * luego el usuario debe definir que accion se tomara con esas
+                 * canciones organizadas.
                  */
                 switch (secondLevelOptions) {
                     case "1":
@@ -141,7 +186,11 @@ public class Main {
                         showSongs(dinamicArray);
                         break;
                 }
-
+                /**
+                 * Ahora el Usuario debe definir que accion se tomara con los
+                 * datos organizados previamente
+                 * Aqui se ingresa al tercer menu.
+                 */
                 showPlaybackMenuByOrder();
                 secondLevelOptions = userData.next();
 
@@ -165,9 +214,25 @@ public class Main {
                         seeLibrary.playSongList();
                         break;
                     case "5":
+                        /**
+                         * Esta opcion cambia la variable de nivel superior
+                         * firstLevelOptions
+                         * de forma que al romper entra al siguiente else if
+                         * que esta en la linea 216
+                         */
                         firstLevelOptions = "5";
                         break;
                 }
+                /**
+                 * Ingreso a la creacion de playlist
+                 * Se crea un ciclo Do para que el usuario pueda interactuar
+                 * con la playlist
+                 *
+                 * -------
+                 * Revisar esta parte para documentarla bien
+                 *
+                 * -----
+                 */
             }else if (firstLevelOptions.equals("4")){
                 if(savePlaylist.getStore().size() > 0){
                     showPlaylistStore(savePlaylist.getStore());
@@ -219,10 +284,10 @@ public class Main {
                                             );
                                         }
                                     }else{
-                                    System.out.println(
-                                            "No existe ninguna playlist"
-                                    );
-                                }
+                                        System.out.println(
+                                                "No existe ninguna playlist"
+                                        );
+                                    }
                                     break;
                             }
                         }
@@ -286,8 +351,8 @@ public class Main {
 
                                     System.out.println(
                                             "______Lista de reproduccion *** " +
-                                            newPlayList.getPlaylistName() +
-                                            " ***_______");
+                                                    newPlayList.getPlaylistName() +
+                                                    " ***_______");
 
                                     showSongs(seeSongs);
                                     System.out.println("Fin lista..........");
@@ -325,12 +390,13 @@ public class Main {
                                     showPlaylistStore(savePlaylist.getStore());
                                     break;
                             }
-                        } while (!(thirdLevelOptions.equalsIgnoreCase("4")));
+                        } while (!(thirdLevelOptions.equalsIgnoreCase(
+                                "4")));
 
                     } catch (NullPointerException e) {
                         System.out.println(
                                 "\nEstamos presentando inconvenientes con la" +
-                                " lista de Canciones:::::::::::::\n");
+                                        " lista de Canciones:::::::::::::\n");
 
                         System.out.println("INTENTE EN OTRO MOMENTO::::::::\n");
                     }
@@ -348,18 +414,34 @@ public class Main {
 
     }
 
+    /**
+     * Este metodo se encarga de mostrarle al usuario la playlist que ha creado
+     * Con un ciclo for recorre el ArrayList que el usuario creo con sus
+     * elecciones e imprime cada cancion contenida
+     * @param playlists Es el ArrayList que el usuario llena cuando crea una
+     *                  una nueva playlist, el mismo que se mostrara en este
+     *                  metodo.
+     *
+     * @since La utlima version pues fue añadido en la ultima gran modificacion
+     */
     private static void showPlaylistStore(List<Playlist> playlists) {
         System.out.println("_________PLAYLISTS_________\n");
         for ( int i = 0; i < playlists.size(); i++) {
             System.out.println(
                     "Id: " + (i+1) +
-                    " -----nombre de la playlist: " + playlists.
-                    get(i).getPlaylistName()
+                            " -----nombre de la playlist: " + playlists.
+                            get(i).getPlaylistName()
             );
         }
     }
 
-
+    /**
+     * Metodo para tener un codigo organizado y agrupar la impresion en pantalla
+     * del menu principal
+     *
+     *
+     * @since La utlima version pues fue añadido en la ultima gran modificacion
+     */
     private static void showMenu() {
         System.out.println("Bienvenido a nuestro reproductor de musica \n");
         System.out.println(" ___________________MENU__________________");
@@ -375,6 +457,13 @@ public class Main {
         System.out.println("|_________________________________________|\n");
     }
 
+    /**
+     * Metodo para tener un codigo organizado y agrupar la impresion en pantalla
+     * del menu de "Ordenar canciones" SortMenu
+     *
+     *
+     * @since La utlima version pues fue añadido en la ultima gran modificacion
+     */
     private static void showSortMenu() {
         System.out.println(" ___________________Opciones____________________");
         System.out.println("|Ordenar Por: __________________________________|");
@@ -391,6 +480,13 @@ public class Main {
         System.out.println("|_______________________________________________|");
     }
 
+    /**
+     * Metodo para tener un codigo organizado y agrupar la impresion en pantalla
+     * del menu de las playlists
+     *
+     *
+     * @since La utlima version pues fue añadido en la ultima gran modificacion
+     */
     private static void playlistOptionsMenu(){
         System.out.println(" \n_____Opciones de PlayList_____");
         System.out.println("| _____________________________|");
@@ -403,6 +499,13 @@ public class Main {
         System.out.println("|______________________________|\n");
     }
 
+    /**
+     * Metodo para tener un codigo organizado y agrupar la impresion en pantalla
+     * del menu que aparece una vez ha ordenado las canciones a voluntad
+     *
+     *
+     * @since La utlima version pues fue añadido en la ultima gran modificacion
+     */
     private static void showPlaybackMenuByOrder() {
         System.out.println("________________Opciones__________________");
         System.out.println("| --------------------------------------- |");
@@ -417,15 +520,29 @@ public class Main {
         System.out.println("|_________________________________________|");
     }
 
+    /**
+     * Este metodo sirve para mostrar de una forma organizada cualquier
+     * seleccion de canciones que haya realizado el usuario.
+     *
+     * Para no mostrar toda la informacion de la cancion y generar una mejor
+     * calidad visual se decidio mostrar solo el Id, titulo, Genero, Duracion y
+     * fecha
+     * @param songs Es la Lista de Arraylists que se ingresa al metodo para ser
+     *              organizado con el ciclo for e imprimir solo la informacion
+     *              necesaria.
+     *
+     * @since La utlima version pues fue añadido en la ultima gran modificacion
+     *
+     */
     private static void showSongs(List<ArrayList<String>> songs){
         for (int i = 0; i < songs.size(); i++) {
             System.out.println(
                     " => " +
-                    " Id: " + songs.get(i).get(0) + "_______" +
-                    " Titulo: " + songs.get(i).get(1) + "_______" +
-                    " Genero: " + songs.get(i).get(2) + "_______" +
-                    " Duracion: " + songs.get(i).get(5) + "_______" +
-                    " Fecha: " + songs.get(i).get(6));
+                            " Id: " + songs.get(i).get(0) + "_______" +
+                            " Titulo: " + songs.get(i).get(1) + "_______" +
+                            " Genero: " + songs.get(i).get(2) + "_______" +
+                            " Duracion: " + songs.get(i).get(5) + "_______" +
+                            " Fecha: " + songs.get(i).get(6));
         }
     }
 }
